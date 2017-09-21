27340fbd37dd4ab45ed30a07a4d077652fb97228
Alexandre Figueiredo <17xande@gmail.com>
2017-09-21 11:27:22 +0200

    basic web server logic

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	log.Println("the go feeder")
+	addr := flag.String("addr", ":8080", "The address of the application")
+	debug := flag.Bool("debug", false, "get more logs")
+
+	if *debug {
+		log.Println("debug mode enabled")
+	}
+
+	data, err := ioutil.ReadFile("configs/config.json")
+	if err != nil {
+		log.Fatal("error reading config file: ", err)
+	}
+
+	err = json.Unmarshal(data, &conf)
+	if err != nil {
+		log.Fatal("error unmarshalling config file: ", err)
+	}
+
+	log.Printf("Listening on port %s\n", *addr)
+	err = http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("could not start web server: ", err)
+	}
 }
